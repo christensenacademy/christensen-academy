5fc63798971e484c4271a909e6b4228993959ba9
Cameron Christensen <cameron@christensenacademy.org>
2013-03-03 18:57:14 -0600

    updates to css layouts

--- modules/how-to-use-this-site/module.php
+++ modules/how-to-use-this-site/module.php
@@ -6,12 +6,18 @@ $map_height = '30';
 $module_playlist = '';
 
 $module_challenges = array(
+# Choose a module
+# Complete a challenge
+# Build your portfolio
+# Complete a module
 );
 
 $module_videos = array(
+# Challenge Maps, Video Playlists, and Textpages
 );
 
 $module_textpages = array(
+new textpage('challenges-videos-textpages.md','Challenge Maps, Video Playlists, and Textpages'),
 );
 
 include('./modules/module-template.php');
